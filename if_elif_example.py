77099b0a7aba23ebdf1a5e820b5dddd584d44906
Mark Ingram <markingramtester@gmail.com>
2018-11-01 19:12:24 +0000

    Next section

---
+++ if_elif_example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+	print ('Hi Alice')
+elif age < 12:
+	print ('You are not Alice, kiddo')
+elif age > 2000:
+	print ('Unlike you Alice is alive')
+elif age > 100:
+	print ('You are not Alice, grannie')
